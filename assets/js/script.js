3ec2a894d492e7e91a09a0406da6db4882812926
AndyWright360 <>
2023-11-16 20:36:06 +0000

    feat: addCard function created to populate game board

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,22 @@
-const cards = document.querySelectorAll(".card-container");
+const cardContainers = document.getElementsByClassName("card-container");
+const cards = ["clover", "crystal", "feather", "frog", "moth", "mushroom", "pouch", "snake", "spider"];
+const allCards = [...cards, ...cards];
 
-cards.forEach(function(card) {
-    card.addEventListener("click", function () {
-        card.classList.toggle("flip");
-    });
-});
+// loop through each card-container div to toggle the flip class on click.
+for (let i = 0; i < cardContainers.length; i++) {
+  cardContainers[i].addEventListener('click', function() {
+    this.classList.toggle('flip');
+  });
+};
+
+// append a new front card image inside each card-container div.
+const addCards = () => {
+  for (i = 0; i < cardContainers.length; i++) {
+    let cardFront = document.createElement('img');
+    cardFront.setAttribute('class', 'game-card front');
+    cardFront.setAttribute('src', `assets/images/${allCards[i]}.png`);
+    cardContainers[i].appendChild(cardFront);
+  };
+};
+
+addCards();
